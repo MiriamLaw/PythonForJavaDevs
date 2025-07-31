9feefa977fda196c0c2a3b07711aefc3a8a4292a
MiriamLaw <mimsical999@yahoo.com>
2025-07-31 10:22:22 -0700

    Update funcmeth.py

---
+++ funcmeth.py
@@ -0,0 +1,17 @@
+animals = 7, 2, 7, 4, 9, 5, 4 #(can use parenthesis around tuples)
+
+print(len(animals))
+print(max(animals))
+print(min(animals))
+
+print(animals.count(4))
+print(animals.index(4))
+
+text = "It was the best of times"
+
+print(len(text))
+print(max(text))
+print(min(text))
+
+print(text.count("w"))
+print(text.index("t"))
